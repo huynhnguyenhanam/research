45bd668eff28c69d4ad656a40a2fd5213ff09c36
huynhnguyenhanam <hanampublic@gmail.com>
2021-10-26 15:57:24 +0700

    mearn stack with blog project

---
+++ my-blog/src/App.js
@@ -0,0 +1,23 @@
+import { BrowserRouter as Router, Route } from "react-router-dom";
+// Components
+import Home from "./pages/Home";
+import About from "./pages/About";
+import ArticlesList from "./pages/ArticlesList";
+import Article from "./pages/Article";
+import NavBar from "./components/NavBar";
+
+function App() {
+  return (
+    <Router>
+      <NavBar />
+      <div className="max-w-screen-md mx-auto pt-20">
+        <Route exact path="/" component={Home} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/articles-list" component={ArticlesList} />
+        <Route exact path="/article/:name" component={Article} />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ my-blog/src/pages/About.js
@@ -0,0 +1,41 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="mb-20">
+      <h1 className="sm:text-4xl text-2xl font-bold mt-6 mb-6 text-gray-900">
+        About
+      </h1>
+      <p className="mx-auto leading-relaxed text-base mb-4">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Exercitationem
+        nulla non iure rerum ratione dolorum nisi deserunt modi qui ipsa
+        molestias asperiores repellendus repellat, fugit sunt cupiditate
+        accusamus a suscipit.
+      </p>
+      <p className="mx-auto leading-relaxed text-base mb-4">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque
+        laudantium exercitationem maxime tempora praesentium impedit deserunt
+        temporibus dolore non numquam corrupti, accusamus aliquam nam quod
+        repudiandae corporis amet nesciunt adipisci distinctio molestias
+        provident doloremque alias fuga magni? Commodi maxime voluptatem sequi
+        pariatur hic quis magnam dolorem similique. Voluptatibus, repellat non!
+      </p>
+      <p className="mx-auto leading-relaxed text-base mb-4">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur
+        ullam maiores ab adipisci tenetur. Voluptate a, amet obcaecati excepturi
+        ex necessitatibus laudantium provident cumque. Qui velit voluptatibus
+        perspiciatis doloremque quis libero explicabo odio officiis adipisci
+        expedita veritatis eligendi voluptas id, voluptatem numquam impedit
+        nesciunt porro ipsum illo tenetur beatae, laborum magnam. Officia unde
+        delectus molestias aspernatur et ut voluptates repellat corporis sint
+        minus accusamus, provident, voluptatem distinctio quae sapiente! Alias
+        error fugiat cumque animi soluta corporis reiciendis recusandae magnam
+        blanditiis culpa architecto est in, aspernatur doloremque maxime atque
+        praesentium sed dolores quis! Commodi eveniet incidunt adipisci
+        molestiae suscipit laborum voluptate.
+      </p>
+    </div>
+  );
+};
+
+export default About;
